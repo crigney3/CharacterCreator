a1d9af86847a66a600bb78ad888bbd122ba381d6
Corey Rigney <coreyrigney@gmail.com>
2019-08-30 12:44:34 -0400

    Added Size and Alignment Enums, more character vars

--- CharacterCreationTest/Character.cs
+++ CharacterCreationTest/Character.cs
@@ -2,12 +2,40 @@
 using System.Collections.Generic;
 namespace CharacterCreationTest
 {
+    public enum SizeEnum
+    {
+        Tiny,
+        Small,
+        Medium,
+        Large,
+        Huge,
+        Gargantuan
+    }
+
+    public enum AlignmentEnum
+    {
+        LawfulGood,
+        NeutralGood,
+        ChaoticGood,
+        LawfulNeutral,
+        TrueNeutral,
+        ChaoticNeutral,
+        LawfulEvil,
+        NeutralEvil,
+        ChaoticEvil
+    }
+
     public class Character
     {
         private Race charRace;
         private Class charClass;
         private Dictionary<string, int> stats;
         private Dictionary<string, int> modifiers;
+        private string name;
+        private string playerName;
+        private int height;
+        private int weight;
+        private SizeEnum size;
 
         public Character()
         {
